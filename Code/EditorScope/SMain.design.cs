9b045f06386fbeec53c8b1ff58c9637019e12aaf
yamiwamiyu <359602182@qq.com>
2019-07-12 17:33:11 +0800

    Scope编辑器

---
+++ Code/EditorScope/SMain.design.cs
@@ -0,0 +1,280 @@
+﻿using System.Collections.Generic;
+using EntryEngine;
+using EntryEngine.UI;
+
+public partial class SMain
+{
+    public EntryEngine.UI.TextureBox TB190712145935 = new EntryEngine.UI.TextureBox();
+    public EntryEngine.UI.Label[] L190712150313 = new EntryEngine.UI.Label[4]
+    {
+        new EntryEngine.UI.Label(),
+        new EntryEngine.UI.Label(),
+        new EntryEngine.UI.Label(),
+        new EntryEngine.UI.Label(),
+    };
+    public EntryEngine.UI.TextBox TBResult = new EntryEngine.UI.TextBox();
+    public EntryEngine.UI.Button BSave = new EntryEngine.UI.Button();
+    public EntryEngine.UI.TextureBox TBCanvas = new EntryEngine.UI.TextureBox();
+    public EntryEngine.UI.DropDown DDAction = new EntryEngine.UI.DropDown();
+    
+    
+    private void Initialize()
+    {
+        this.Name = "SMain";
+        EntryEngine.RECT this_Clip = new EntryEngine.RECT();
+        this_Clip.X = 0f;
+        this_Clip.Y = 0f;
+        this_Clip.Width = 1280f;
+        this_Clip.Height = 720f;
+        this.Clip = this_Clip;
+        
+        TB190712145935.Name = "TB190712145935";
+        EntryEngine.RECT TB190712145935_Clip = new EntryEngine.RECT();
+        TB190712145935_Clip.X = 800f;
+        TB190712145935_Clip.Y = 0f;
+        TB190712145935_Clip.Width = 480f;
+        TB190712145935_Clip.Height = 720f;
+        TB190712145935.Clip = TB190712145935_Clip;
+        
+        TB190712145935.Color = new COLOR()
+        {
+            B = 128,
+            G = 128,
+            R = 128,
+            A = 255,
+        };
+        this.Add(TB190712145935);
+        L190712150313[0].Name = "L190712150313";
+        EntryEngine.RECT L190712150313_0__Clip = new EntryEngine.RECT();
+        L190712150313_0__Clip.X = 18f;
+        L190712150313_0__Clip.Y = 623.375f;
+        L190712150313_0__Clip.Width = 446f;
+        L190712150313_0__Clip.Height = 0;
+        L190712150313[0].Clip = L190712150313_0__Clip;
+        
+        L190712150313[0].UIText = new EntryEngine.UI.UIText();
+        L190712150313[0].UIText.Text = "左键拖拽或创建矩形";
+        L190712150313[0].UIText.FontColor = new COLOR()
+        {
+            B = 255,
+            G = 255,
+            R = 255,
+            A = 255,
+        };
+        L190712150313[0].UIText.TextAlignment = (EPivot)0;
+        L190712150313[0].UIText.TextShader = null;
+        L190712150313[0].UIText.Padding.X = 0f;
+        L190712150313[0].UIText.Padding.Y = 0f;
+        L190712150313[0].UIText.FontSize = 16f;
+        L190712150313[0].Text = "左键拖拽或创建矩形";
+        TB190712145935.Add(L190712150313[0]);
+        L190712150313[1].Name = "L190712150313";
+        EntryEngine.RECT L190712150313_1__Clip = new EntryEngine.RECT();
+        L190712150313_1__Clip.X = 17f;
+        L190712150313_1__Clip.Y = 641.625f;
+        L190712150313_1__Clip.Width = 446f;
+        L190712150313_1__Clip.Height = 0;
+        L190712150313[1].Clip = L190712150313_1__Clip;
+        
+        L190712150313[1].UIText = new EntryEngine.UI.UIText();
+        L190712150313[1].UIText.Text = "右键拖拽画布或删除矩形";
+        L190712150313[1].UIText.FontColor = new COLOR()
+        {
+            B = 255,
+            G = 255,
+            R = 255,
+            A = 255,
+        };
+        L190712150313[1].UIText.TextAlignment = (EPivot)0;
+        L190712150313[1].UIText.TextShader = null;
+        L190712150313[1].UIText.Padding.X = 0f;
+        L190712150313[1].UIText.Padding.Y = 0f;
+        L190712150313[1].UIText.FontSize = 16f;
+        L190712150313[1].Text = "右键拖拽画布或删除矩形";
+        TB190712145935.Add(L190712150313[1]);
+        L190712150313[2].Name = "L190712150313";
+        EntryEngine.RECT L190712150313_2__Clip = new EntryEngine.RECT();
+        L190712150313_2__Clip.X = 17f;
+        L190712150313_2__Clip.Y = 659.875f;
+        L190712150313_2__Clip.Width = 446f;
+        L190712150313_2__Clip.Height = 0;
+        L190712150313[2].Clip = L190712150313_2__Clip;
+        
+        L190712150313[2].UIText = new EntryEngine.UI.UIText();
+        L190712150313[2].UIText.Text = "空格重置视口";
+        L190712150313[2].UIText.FontColor = new COLOR()
+        {
+            B = 255,
+            G = 255,
+            R = 255,
+            A = 255,
+        };
+        L190712150313[2].UIText.TextAlignment = (EPivot)0;
+        L190712150313[2].UIText.TextShader = null;
+        L190712150313[2].UIText.Padding.X = 0f;
+        L190712150313[2].UIText.Padding.Y = 0f;
+        L190712150313[2].UIText.FontSize = 16f;
+        L190712150313[2].Text = "空格重置视口";
+        TB190712145935.Add(L190712150313[2]);
+        L190712150313[3].Name = "L190712150313";
+        EntryEngine.RECT L190712150313_3__Clip = new EntryEngine.RECT();
+        L190712150313_3__Clip.X = 17f;
+        L190712150313_3__Clip.Y = 678.125f;
+        L190712150313_3__Clip.Width = 446f;
+        L190712150313_3__Clip.Height = 0;
+        L190712150313[3].Clip = L190712150313_3__Clip;
+        
+        L190712150313[3].UIText = new EntryEngine.UI.UIText();
+        L190712150313[3].UIText.Text = "拖拽动作文件到视口加载动作";
+        L190712150313[3].UIText.FontColor = new COLOR()
+        {
+            B = 255,
+            G = 255,
+            R = 255,
+            A = 255,
+        };
+        L190712150313[3].UIText.TextAlignment = (EPivot)0;
+        L190712150313[3].UIText.TextShader = null;
+        L190712150313[3].UIText.Padding.X = 0f;
+        L190712150313[3].UIText.Padding.Y = 0f;
+        L190712150313[3].UIText.FontSize = 16f;
+        L190712150313[3].Text = "拖拽动作文件到视口加载动作";
+        TB190712145935.Add(L190712150313[3]);
+        TBResult.Name = "TBResult";
+        EntryEngine.RECT TBResult_Clip = new EntryEngine.RECT();
+        TBResult_Clip.X = 18f;
+        TBResult_Clip.Y = 18.375f;
+        TBResult_Clip.Width = 446f;
+        TBResult_Clip.Height = 545f;
+        TBResult.Clip = TBResult_Clip;
+        
+        TBResult.DefaultText = new EntryEngine.UI.UIText();
+        TBResult.DefaultText.Text = "";
+        TBResult.DefaultText.FontColor = new COLOR()
+        {
+            B = 211,
+            G = 211,
+            R = 211,
+            A = 255,
+        };
+        TBResult.DefaultText.TextAlignment = (EPivot)0;
+        TBResult.DefaultText.TextShader = null;
+        TBResult.DefaultText.Padding.X = 0f;
+        TBResult.DefaultText.Padding.Y = 0f;
+        TBResult.DefaultText.FontSize = 16f;
+        TBResult.UIText = new EntryEngine.UI.UIText();
+        TBResult.UIText.Text = "";
+        TBResult.UIText.FontColor = new COLOR()
+        {
+            B = 0,
+            G = 0,
+            R = 0,
+            A = 255,
+        };
+        TBResult.UIText.TextAlignment = (EPivot)0;
+        TBResult.UIText.TextShader = null;
+        TBResult.UIText.Padding.X = 20f;
+        TBResult.UIText.Padding.Y = 20f;
+        TBResult.UIText.FontSize = 16f;
+        TBResult.BreakLine = true;
+        TB190712145935.Add(TBResult);
+        BSave.Name = "BSave";
+        EntryEngine.RECT BSave_Clip = new EntryEngine.RECT();
+        BSave_Clip.X = 191f;
+        BSave_Clip.Y = 567.375f;
+        BSave_Clip.Width = 100f;
+        BSave_Clip.Height = 44f;
+        BSave.Clip = BSave_Clip;
+        
+        BSave.Color = new COLOR()
+        {
+            B = 255,
+            G = 255,
+            R = 255,
+            A = 55,
+        };
+        BSave.UIText = new EntryEngine.UI.UIText();
+        BSave.UIText.Text = "复制";
+        BSave.UIText.FontColor = new COLOR()
+        {
+            B = 255,
+            G = 255,
+            R = 255,
+            A = 255,
+        };
+        BSave.UIText.TextAlignment = (EPivot)17;
+        BSave.UIText.TextShader = null;
+        BSave.UIText.Padding.X = 0f;
+        BSave.UIText.Padding.Y = 0f;
+        BSave.UIText.FontSize = 16f;
+        BSave.Text = "复制";
+        TB190712145935.Add(BSave);
+        TBCanvas.Name = "TBCanvas";
+        EntryEngine.RECT TBCanvas_Clip = new EntryEngine.RECT();
+        TBCanvas_Clip.X = 0f;
+        TBCanvas_Clip.Y = 0f;
+        TBCanvas_Clip.Width = 800f;
+        TBCanvas_Clip.Height = 720f;
+        TBCanvas.Clip = TBCanvas_Clip;
+        
+        this.Add(TBCanvas);
+        DDAction.Name = "DDAction";
+        EntryEngine.RECT DDAction_Clip = new EntryEngine.RECT();
+        DDAction_Clip.X = 640f;
+        DDAction_Clip.Y = 18.375f;
+        DDAction_Clip.Width = 150f;
+        DDAction_Clip.Height = 31f;
+        DDAction.Clip = DDAction_Clip;
+        
+        DDAction.Color = new COLOR()
+        {
+            B = 255,
+            G = 255,
+            R = 255,
+            A = 128,
+        };
+        DDAction.UIText = new EntryEngine.UI.UIText();
+        DDAction.UIText.Text = "";
+        DDAction.UIText.FontColor = new COLOR()
+        {
+            B = 255,
+            G = 255,
+            R = 255,
+            A = 255,
+        };
+        DDAction.UIText.TextAlignment = (EPivot)17;
+        DDAction.UIText.TextShader = null;
+        DDAction.UIText.Padding.X = 0f;
+        DDAction.UIText.Padding.Y = 0f;
+        DDAction.UIText.FontSize = 16f;
+        TBCanvas.Add(DDAction);
+        
+        this.PhaseShowing += Show;
+    }
+    protected override IEnumerable<ICoroutine> Loading()
+    {
+        ICoroutine async;
+        ICoroutine ___async;
+        TB190712145935.Texture = PATCH.GetNinePatch(PATCH.NullColor, new COLOR(192, 192, 192, 255), 2);
+        
+        
+        
+        
+        TBResult.SourceNormal = PATCH.GetNinePatch(PATCH.NullColor, new COLOR(0, 0, 0, 255), 1);
+        
+        
+        BSave.SourceNormal = PATCH.GetNinePatch(PATCH.NullColor, new COLOR(0, 0, 0, 255), 2);
+        
+        DDAction.SourceNormal = PATCH.GetNinePatch(PATCH.NullColor, new COLOR(255, 255, 0, 255), 2);
+        
+        
+        var __loading = MyLoading();
+        if (__loading != null)
+        foreach (var item in __loading)
+        yield return item;
+    }
+    private void Show(EntryEngine.UI.UIScene __scene)
+    {
+        
+    }
+}
---
+++ Code/EditorScope/SMain.logic.cs
@@ -0,0 +1,237 @@
+﻿using System.Collections.Generic;
+using EntryEngine;
+using EntryEngine.UI;
+using System;
+using System.Text;
+
+public partial class SMain : UIScene
+{
+    public static Action<string[]> DragFile;
+
+    MATRIX2x3 offset;
+    MATRIX2x3 offsetInvert;
+    ANIMATION animation;
+    List<RECT> rects = new List<RECT>();
+    VECTOR2 start = new VECTOR2(float.NaN, 0);
+    VECTOR2 dragOffset = new VECTOR2(float.NaN, 0);
+    int dragIndex = -1;
+    TEXTURE area;
+
+    bool Creating { get { return !float.IsNaN(start.X); } }
+
+    public SMain()
+    {
+        Initialize();
+        TBResult.TextEditOver += new Action<Label>(TBResult_TextEditOver);
+        BSave.Clicked += new DUpdate<UIElement>(BSave_Clicked);
+    }
+
+    void Copy()
+    {
+        Entry.INPUT.InputDevice.Copy(TBResult.Text);
+    }
+    void BSave_Clicked(UIElement sender, Entry e)
+    {
+        Copy();
+    }
+    void TBResult_TextEditOver(Label obj)
+    {
+        string origin = TBResult.Text;
+        if (string.IsNullOrEmpty(origin))
+        {
+            rects.Clear();
+            return;
+        }
+
+        try
+        {
+            List<RECT> temp = new List<RECT>();
+            string[] datas = origin.Split('|');
+            for (int i = 0; i < datas.Length; i++)
+            {
+                string[] param = datas[i].Split(';');
+                temp.Add(new RECT(int.Parse(param[0]), int.Parse(param[1]), int.Parse(param[2]), int.Parse(param[3])));
+            }
+            this.rects = temp;
+        }
+        catch (Exception)
+        {
+            Save();
+        }
+    }
+    void Save()
+    {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0, n = rects.Count - 1; i <= n; i++)
+        {
+            var rect = rects[i];
+            builder.AppendFormat("{0};{1};{2};{3}", (int)rect.X, (int)rect.Y, (int)rect.Width, (int)rect.Height);
+            if (i != n)
+                builder.Append("|");
+        }
+        TBResult.Text = builder.ToString();
+    }
+    
+    private IEnumerable<ICoroutine> MyLoading()
+    {
+        area = PATCH.GetNinePatch(new COLOR(255, 0, 0, 64), new COLOR(255, 0, 0, 222), 1);
+        ResetViewport();
+        return null;
+    }
+
+    void ResetViewport()
+    {
+        offset = MATRIX2x3.Identity;
+        offset.M31 = 400;
+        offset.M32 = 400;
+        MATRIX2x3.Invert(ref offset, out offsetInvert);
+    }
+    VECTOR2 GetCurrentPosition()
+    {
+        var position = Entry.INPUT.Pointer.Position;
+        VECTOR2.Transform(ref position, ref offsetInvert);
+        return position;
+    }
+    int ClickIndex()
+    {
+        VECTOR2 p;
+        return ClickIndex(out p);
+    }
+    int ClickIndex(out VECTOR2 position)
+    {
+        position = GetCurrentPosition();
+        for (int i = rects.Count - 1; i >= 0; i--)
+            if (rects[i].Contains(position))
+                return i;
+        return -1;
+    }
+
+    protected override void InternalUpdate(Entry e)
+    {
+        base.InternalUpdate(e);
+        if (!TBResult.Checked)
+            Save();
+    }
+    protected override void InternalEvent(Entry e)
+    {
+        base.InternalEvent(e);
+        // 空格重置视口
+        if (e.INPUT.Keyboard.IsClick(PCKeys.Space))
+            ResetViewport();
+        // 保存数据到剪切板
+        if (e.INPUT.Keyboard.Ctrl && e.INPUT.Keyboard.IsClick(PCKeys.S))
+            Copy();
+        // 创建或选中时上下左右微调
+        if (dragIndex != -1)
+        {
+            if (e.INPUT.Keyboard.IsInputKeyPressed(PCKeys.A) || e.INPUT.Keyboard.IsInputKeyPressed(PCKeys.Left))
+                dragOffset.X--;
+            if (e.INPUT.Keyboard.IsInputKeyPressed(PCKeys.D) || e.INPUT.Keyboard.IsInputKeyPressed(PCKeys.Right))
+                dragOffset.X++;
+            if (e.INPUT.Keyboard.IsInputKeyPressed(PCKeys.S) || e.INPUT.Keyboard.IsInputKeyPressed(PCKeys.Down))
+                dragOffset.Y++;
+            if (e.INPUT.Keyboard.IsInputKeyPressed(PCKeys.W) || e.INPUT.Keyboard.IsInputKeyPressed(PCKeys.Up))
+                dragOffset.Y--;
+        }
+        //else if (Creating)
+        //{
+        //    if (e.INPUT.Keyboard.IsInputKeyPressed(PCKeys.A) || e.INPUT.Keyboard.IsInputKeyPressed(PCKeys.Left))
+        //        start.X--;
+        //    if (e.INPUT.Keyboard.IsInputKeyPressed(PCKeys.D) || e.INPUT.Keyboard.IsInputKeyPressed(PCKeys.Right))
+        //        start.X++;
+        //    if (e.INPUT.Keyboard.IsInputKeyPressed(PCKeys.S) || e.INPUT.Keyboard.IsInputKeyPressed(PCKeys.Down))
+        //        start.Y++;
+        //    if (e.INPUT.Keyboard.IsInputKeyPressed(PCKeys.W) || e.INPUT.Keyboard.IsInputKeyPressed(PCKeys.Up))
+        //        start.Y--;
+        //}
+        // 左键拖拽/生成矩形
+        if (TBCanvas.IsHover && e.INPUT.Pointer.IsClick(0))
+        {
+            VECTOR2 position;
+            int index = ClickIndex(out position);
+            if (index == -1)
+            {
+                // 创建新矩形
+                start = position;
+            }
+            else
+            {
+                // 拖拽旧矩形
+                dragIndex = index;
+                dragOffset = rects[dragIndex].Location;
+            }
+        }
+
+        if (dragIndex != -1 && e.INPUT.Pointer.IsPressed(0))
+        {
+            // 拖拽
+            rects[dragIndex] = new RECT(e.INPUT.Pointer.Position - e.INPUT.Pointer.ClickPosition + dragOffset, rects[dragIndex].Size);
+        }
+
+        if (e.INPUT.Pointer.IsRelease(0))
+        {
+            if (dragIndex != -1)
+            {
+                // 确认拖拽
+                dragIndex = -1;
+            }
+            else if (Creating)
+            {
+                // 确认创建
+                rects.Add(RECT.CreateRectangle(start, GetCurrentPosition()));
+                start.X = float.NaN;
+            }
+        }
+
+        if (e.INPUT.Pointer.IsClick(1))
+        {
+            if (dragIndex != -1)
+            {
+                // 取消拖拽
+                rects[dragIndex] = new RECT(e.INPUT.Pointer.ClickPosition - dragOffset, rects[dragIndex].Size);
+                dragIndex = -1;
+            }
+            else if (Creating)
+            {
+                // 取消创建新矩形
+                start.X = float.NaN;
+            }
+            else
+            {
+                // 删除矩形
+                int index = ClickIndex();
+                if (index != -1)
+                    rects.RemoveAt(index);
+            }
+        }
+
+        if (e.INPUT.Pointer.IsPressed(1))
+        {
+            // 拖拽地图
+            var delta = e.INPUT.Pointer.DeltaPosition;
+            offset.M31 += delta.X;
+            offset.M32 += delta.Y;
+            MATRIX2x3.Invert(ref offset, out offsetInvert);
+        }
+    }
+    protected override void InternalDraw(GRAPHICS spriteBatch, Entry e)
+    {
+        base.InternalDraw(spriteBatch, e);
+
+        spriteBatch.Draw(TEXTURE.Pixel, new RECT(0, offset.M32, 800, 1), COLOR.Lime);
+        spriteBatch.Draw(TEXTURE.Pixel, new RECT(offset.M31, 0, 1, 800), COLOR.Lime);
+
+        spriteBatch.Begin(offset);
+
+        if (animation != null)
+            spriteBatch.Draw(animation, VECTOR2.Zero);
+
+        for (int i = 0; i < rects.Count; i++)
+            spriteBatch.Draw(area, rects[i]);
+        if (Creating)
+            spriteBatch.Draw(area, RECT.CreateRectangle(start, GetCurrentPosition()));
+
+        spriteBatch.End();
+        
+    }
+}
